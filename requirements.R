5104c869ce2be9ee4365bf9b29a70241f5da0a66
atyler <tylersvgs@gmail.com>
2018-08-11 18:39:55 -0400

    not
    as

--- requirements.R
+++ requirements.R
@@ -3,9 +3,9 @@ library(remotes)
 ##### Install specific package versions from CRAN #####
 
 install_version("ggplot2", "3.0.0")
-# install_version("dplyr", "0.7.5")
-# install_version("tidyr", "0.8.1")
-# install_version("purrr", "0.2.5")
+install_version("dplyr", "0.7.5")
+install_version("tidyr", "0.8.1")
+install_version("purrr", "0.2.5")
 
 ##### Install packages in development from GitHub #####
 
